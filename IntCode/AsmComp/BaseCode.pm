a6475fda46b9de96a6bcc8474ee5cb1aa5b03fbd
Ronald Lamoreaux <gitkey@chindraba.work>
2019-12-29 09:14:25 +0000

    Add low-level memory routines.
    Two routines, `base_read_memory` and `base_write_memory` are internal
    routines to access the memory using the address mode supplied in the
    arguments.
    
    The `system_memory` is the interface exposed to the using module which
    abstracts the memory access. Of special note are the calling options.:
    
    * `system_memory()` [no args] results in a raw memory dump for program
    ending, diagnostic, other purposes, and returns false.
    * `system_memory(1,15)` [2 args] reads, and returns the contents, of
    the memory referenced by the second argument, potentially modified in
    accordance to the address mode indicated by teh first arg. The first
    argument should be supplied using the given address mode variable, not
    using hard-coded numbers.
    * `system_memory(0,43,4569)` [3 args] writes the 3rd argument to memory
    using the same concept of address mode used for reading memory above.
    The return value is the number written to memory.
    * `system_memory(1,2,3,..,45)` [more than 3 args] copies the list of
    arguments directly into memory, starting at address 0, and returns the
    size of the used memory.
    
    Signed-off-by: Ronald Lamoreaux <gitkey@chindraba.work>

---
+++ IntCode/AsmComp/BaseCode.pm
@@ -0,0 +1,140 @@
+package IntCode::AsmComp::BaseCode;
+
+# SPDX-License-Identifier: MIT
+
+use 5.026001;
+use strict;
+use warnings;
+use IntCode::AsmComp::Internals;
+
+use Data::Dumper qw/Dumper/;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+our %EXPORT_TAGS = ( 'all' => [ qw(
+	system_memory
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = ( @{ $EXPORT_TAGS{'all'} } );
+
+our $VERSION = '0.01.02';
+
+sub base_read_memory {
+# Routine to read memory using the supplied operand and given address mode
+#   Arg 1 is Address mode
+#   Arg_2 is operand
+#   Return is the resolved value
+    my ($addr_mode, $operand) = @_;
+    if ($addr_mode == $address_mode{relative} ) {
+        return $operand + $process_registers{S};
+    } elsif ($addr_mode == $address_mode{absolute} ) {
+        return $core_ram[$operand];
+    } elsif ($addr_mode == $address_mode{indexed} ) {
+        return $core_ram[$process_registers{I} + $operand];
+    } elsif ($addr_mode == $address_mode{pointer} ) {
+        return $core_ram[$core_ram[$operand]];
+    } elsif ($addr_mode == $address_mode{reference} ) {
+        return $core_ram[$core_ram[$process_registers{I} + $operand]];
+    } else {
+        return $operand;
+    }
+}
+
+sub base_write_memory {
+# Routine to write memory using the supplied operand and given address mode
+#   Arg 1 is Address mode
+#   Arg_2 is operand
+#   Arg_3 is the data to write
+    my ($addr_mode, $operand, $data) = @_;
+    if ($addr_mode == $address_mode{absolute} ) {
+        $core_ram[$operand] = $data;
+    } elsif ($addr_mode == $address_mode{indexed} ) {
+        $core_ram[$process_registers{I} + $operand] = $data;
+    } elsif ($addr_mode == $address_mode{pointer} ) {
+        $core_ram[$core_ram[$operand]] = $data;
+    } elsif ($addr_mode == $address_mode{reference} ) {
+        $core_ram[$core_ram[$process_registers{I} + $operand]] = $data;
+    } else {
+        return undef;
+    }
+    return 1;
+}
+
+sub system_memory {
+# Routine to access the system memory
+# Two arg version reads memory
+# Three arg version writes memory
+# List longer than 3 args is dumped into memory raw, initialize it
+# Arg 1: memory address mode
+# Arg 2: memory address, will be modified as per address mode
+# Arg 3: if present, the data to write to memory
+    if ( 0 == @_ ) {
+        my $memory_dump = join ', ', ($core_ram);
+        say "Memory dump: $memory_dump";
+    } elsif ( 3 < @_ ) {
+        @core_ram = @_;
+    } elsif ( 3 == @_ ) {
+        return base_write_memory( @_ );
+    } elsif ( 2 == @_ ) {
+        return base_read_memory( @_ );
+    } else {
+        die "Attempt to access memory with invalid parameter(s): '", join("', '", (@_)),"'\n";
+    }
+}
+
+1;
+__END__
+
+=head1 NAME
+
+IntCode::AsmComp::BaseCode
+
+=head1 SYNOPSIS
+
+  use IntCode::AsmComp::BaseCode;
+
+=head1 DESCRIPTION
+
+Implementation of the low-level functionality of the IntCode computer
+needed by the elves in the 2019 Advent of Code challenges.
+
+=head2 EXPORT
+
+None by default.
+
+=head1 AUTHOR
+
+Chindraba, E<lt>aoc@chindraba.workE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2019  Chindraba (Ronald Lamoreaux)
+                  <aoc@chindraba.work>
+- All Rights Reserved
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+=cut
