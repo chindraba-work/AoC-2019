6f75a01aef8bc5a23b7b4febd0dbfa14b5c0abb3
Ronald Lamoreaux <gitkey@chindraba.work>
2020-01-02 03:19:53 +0000

    Internalize the running of the code.
    Switch from the use of the opcodes as the interface to a list of codes
    to run as the interface. Now the commands are passed in as a list of
    values, the code to run and when needed the access mode and operand. The
    module handles interating through the list, including knowing how many
    of the list elements are used for each instruction.
    
    The only exported symbols are now the routine to run the commands and a
    routine for loading the initial state of the memory.
    
    Signed-off-by: Ronald Lamoreaux <gitkey@chindraba.work>

--- IntCode/AsmComp/OpCodes.pm
+++ IntCode/AsmComp/OpCodes.pm
@@ -12,7 +12,8 @@ require Exporter;
 our @ISA = qw(Exporter);
 
 our %EXPORT_TAGS = ( 'op_codes' => [ qw(
-    %opcode
+    load_memory
+    run_snippet
 ) ] );
 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'op_codes'} } );
@@ -21,9 +22,34 @@ our @EXPORT = ( @{ $EXPORT_TAGS{'op_codes'} } );
 
 our $VERSION = '0.01.02';
 
+my @snippet;
+my $access_mode;
+my $operand;
+
+sub get_memory {
+    $access_mode = shift @snippet;
+    $operand = shift @snippet;
+    return system_memory $access_mode, $operand;
+}
+
+sub set_memory {
+    system_memory $access_mode, $operand, $_[0];
+}
+
+sub flag_test {
+    system_flag('N', undef, $_[0]);
+    system_flag('Z', undef, $_[0]);
+}
+
 sub branch {
-    system_register 'C', 2 + (system_register 'C') + system_memory (@_[2,3])
+    system_register('C', 2 + (system_register 'C') + $_[2])
         if ( $_[0] == system_flag $_[1] );
+}
+
+sub copy_register {
+    system_register $_[1], system_register $_[0];
+    flag_test(system_register $_[1])
+        if $_[2];
 }
 
 sub core_dump {
@@ -33,56 +59,53 @@ sub core_dump {
     system_flag;
 }
 
-sub flag_test {
-    system_flag('N', undef, $_[0]);
-    system_flag('Z', undef, $_[0]);
-}
-
-sub copy_register {
-    system_register $_[1], system_register $_[0];
-    flag_test system_register $_[1] if ( $_[2] );
-}
-
 sub load_register {
-    system_register $_[2], system_memory $_[0], $_[1];
-    flag_test system_register $_[2]
-        if $_[3];
+    system_register $_[0], get_memory;
+    flag_test(system_register $_[0])
+        if $_[1];
+}
+
+sub store_register {
+    $access_mode = shift @snippet;
+    $operand = shift @snippet;
+    set_memory system_register($_[0]);
 }
 
 # Routines to implement the Opcodes in a common manner
-our %opcode = (
+my %opcode = (
     ADC => sub {
-        system_register 'A', (system_register 'A') + system_memory @_;
-        flag_test system_register 'A';
+        system_register 'A', (system_register 'A') + get_memory;
+        flag_test(system_register 'A');
     },
     AND => sub {
-        system_register 'A', (system_register 'A') & system_memory @_;
-        flag_test system_register 'A';
+        system_register 'A', (system_register 'A') & get_memory;
+        flag_test(system_register 'A');
     },
     ASL => sub {
-        system_register 'D', system_memory @_;
+        system_register 'D', get_memory;
         system_flag 'C', undef, (system_register 'D') & (~0^~0>>1);
         system_register 'D', (system_register 'D') << 1;
-        system_memory @_, system_register 'D';
-        flag_test system_register 'D';
+        set_memory system_register 'D';
+        flag_test(system_register 'D');
     },
     ASR => sub {
-        system_register 'D', (system_memory @_) & (~0^~0>>1);
-        system_memory @_, (system_register 'D') | ((system_memory @_) >> 1);
-        flag_test system_memory @_;
+        my $value = get_memory;
+        system_register 'D', $value & (~0^~0>>1);
+        set_memory( (system_register 'D') | ($value >> 1));
+        flag_test(system_memory $access_mode, $operand);
     },
-    BCC => sub { branch 0, 'C', @_; },
-    BCS => sub { branch 1, 'C', @_; },
-    BEQ => sub { branch 1, 'Z', @_; },
+    BCC => sub { branch(0, 'C', @_); },
+    BCS => sub { branch(1, 'C', @_); },
+    BEQ => sub { branch(1, 'Z', @_); },
     BIT => sub {
-        system_register 'D', system_memory @_;
+        system_register 'D', get_memory;
         system_flag 'N', undef, system_register 'D';
         system_register 'D', (system_register 'D') & system_register 'A';
         system_flag 'Z', undef, system_register 'D';
     },
-    BMI => sub { branch 1, 'N', @_; },
-    BNE => sub { branch 0, 'Z', @_; },
-    BPL => sub { branch 0, 'N', @_; },
+    BMI => sub { branch(1, 'N', get_memory); },
+    BNE => sub { branch(0, 'Z', get_memory); },
+    BPL => sub { branch(0, 'N', get_memory); },
     BRK => sub {
         system_flag 'I', 1;
         system_stack 2 + system_register 'C';
@@ -93,93 +116,101 @@ our %opcode = (
         system_flag;
         exit;
     },
-    BVC => sub { branch 0, 'V', @_; },
-    BVS => sub { branch 1, 'V', @_; },
+    BVC => sub { branch(0, 'V', get_memory); },
+    BVS => sub { branch(1, 'V', get_memory); },
     CLC => sub { system_flag 'C', 0; },
     CLD => sub { system_flag 'D', 0; },
     CLI => sub { system_flag 'I', 0; },
     CLV => sub { system_flag 'V', 0; },
     CMP => sub {
-        system_register 'D', (system_register 'A') - system_memory @_;
-        flag_test system_register 'D';
+        system_register 'D', (system_register 'A') - get_memory;
+        flag_test(system_register 'D');
     },
     CPX => sub {
-        system_register 'D', (system_register 'X') - system_memory @_;
-        flag_test system_register 'D';
+        system_register 'D', (system_register 'X') - get_memory;
+        flag_test(system_register 'D');
     },
     DEC => sub {
-        system_memory @_, -1 + system_memory @_;
-        flag_test system_memory @_;
+        my $value = get_memory;
+        system_memory $access_mode, $operand, -1 + $value;
+        flag_test(system_memory $access_mode, $operand);
     },
     DEX => sub {
         system_register 'X', -1 + system_register 'X';
-        flag_test system_register 'X';
+        flag_test(system_register 'X');
     },
     EOR => sub {
-        system_register 'A', (system_register 'A') ^ system_memory @_;
-        flag_test system_register 'A';
+        system_register 'A', (system_register 'A') ^ get_memory;
+        flag_test(system_register 'A');
     },
     INC => sub {
-        system_memory @_, 1 + system_memory @_;
-        flag_test system_memory @_;
+        my $value = get_memory;
+        system_memory $access_mode, $operand, 1 + $value;
+        flag_test(system_memory $access_mode, $operand);
     },
     INP => sub {
-        my ($addr_mode, $operand) = @_;
+        get_memory;
         system_register 'D', shift @ARGV
             || die "Data expected but not found on command line.";
-        flag_test system_register 'D';
-        if ( $addr_mode == $addressing{Accumulator} ) {
+        flag_test(system_register 'D');
+        if ( $access_mode == $addressing{Accumulator} ) {
             system_register 'A', system_register 'D';
         } else {
-            system_memory @_, system_register 'D';
+            system_memory $access_mode, $operand, system_register 'D';
         }
     },
     INX => sub {
         system_register 'X', 1 + system_register 'X';
-        flag_test system_register 'X';
+        flag_test(system_register 'X');
     },
-    JMP => sub { system_register 'C', system_memory @_; },
+    JMP => sub { system_register 'C', get_memory; },
     JSR => sub {
         system_stack 2 + system_register 'C';
-        system_register 'C', system_memory @_;
+        system_register 'C', get_memory;
     },
-    LDA => sub { load_register @_, 'A', 1; },
-    LDF => sub { load_register @_, 'F'; },
-    LDI => sub { load_register @_, 'I', 1; },
-    LDS => sub { load_register @_, 'S', 1; },
-    LDX => sub { load_register @_, 'X', 1; },
+    LDA => sub { load_register('A', 1); },
+    LDF => sub { load_register('F'); },
+    LDI => sub { load_register('I', 1); },
+    LDS => sub { load_register('S', 1); },
+    LDX => sub { load_register('X', 1); },
     LSR => sub {
-        system_memory @_, (system_memory @_ >> 1);
-        flag_test system_memory @_;
+        set_memory(get_memory >> 1);
+        flag_test(system_memory $access_mode, $operand);
     },
     MUL => sub {
-        system_register 'A', (system_register 'A') * system_memory @_;
-        flag_test system_register 'A';
+        system_register 'A', (system_register 'A') * get_memory;
+        flag_test(system_register 'A');
     },
     ORA => sub {
-        system_register 'A', (system_register 'A') | system_memory @_;
-        flag_test system_register 'A';
+        system_register 'A', (system_register 'A') | get_memory;
+        flag_test(system_register 'A');
     },
     OUT => sub {
-        my ($addr_mode, $operand) = @_;
-        system_register 'D', system_memory @_;
+        system_register 'D', get_memory;
         printf "Program output: %1\$016b: %1\$u, (%1\$d)\n", system_register 'D';
     },
     PHA => sub { system_stack system_register 'A'; },
     PHP => sub { system_stack system_register 'F'; },
-    PLA => sub { load_register 1, system_stack, 'A', 1; },
-    PLP => sub { load_register 1, system_stack, 'F'; },
+    PLA => sub { 
+        system_register 'A', system_stack;
+        flag_test(system_register 'A');
+    },
+    PLP => sub { system_register 'F', system_stack; },
     ROL => sub {
-        system_flag 'C', undef, (system_memory @_) & (~0^~0>>1);
-        system_memory @_, ((system_memory @_) << 1) + system_flag 'C';
-        flag_test system_memory @_;
+        my $value = get_memory;
+        system_flag 'C', undef, $value & (~0^~0>>1);
+        set_memory(($value << 1) + system_flag 'C');
+        flag_test(system_memory $access_mode, $operand);
     },
     ROR => sub {
-        system_flag 'C', undef, (system_memory @_) & 1;
-        system_memory @_, (system_memory @_) >> 1;
-        system_memory @_, (system_memory @_) | (~0^~0>>1)
-            if ( system_flag 'C' );
-        flag_test system_memory @_;
+        my $value = get_memory;
+        system_flag 'C', undef, $value & 1;
+        if ( system_flag 'C' ) {
+            set_memory(($value >> 1) | (~0^~0>>1));
+        } else {
+            set_memory $value >> 1;
+        }
+        flag_test(system_memory $access_mode, $operand);
     },
     RTI => sub {
         system_register 'F', system_stack;
@@ -187,7 +218,7 @@ our %opcode = (
     },
     RTS => sub { system_register 'C', system_stack; },
     SBC => sub {
-        system_register 'A', (system_register 'A') - system_memory @_;
+        system_register 'A', (system_register 'A') - get_memory;
         map {
             system_flag($_, undef, system_register('A'));
         } qw(N V Z C);
@@ -195,9 +226,9 @@ our %opcode = (
     SEC => sub { system_flag('C', 1); },
     SED => sub { system_flag('D', 1); },
     SEI => sub { system_flag('I', 1); },
-    STA => sub { system_memory(@_, system_register('A')); },
-    STI => sub { system_memory(@_, system_register('I')); },
-    STX => sub { system_memory(@_, system_register('X')); },
+    STA => sub { store_register 'A'; },
+    STI => sub { store_register 'I'; },
+    STX => sub { store_register 'X'; },
     TAI => sub { copy_register 'A', 'I', 1; },
     TAX => sub { copy_register 'A', 'X', 1; },
     TIA => sub { copy_register 'I', 'A'; },
@@ -209,16 +240,29 @@ our %opcode = (
 );
 
 $opcode{DIV} = sub {
-        if ( 0 == system_memory @_ ) {
-            system_flag('Z', 1);
-            system_flag('V', 1);
-            system_flag('B', 1);
-            $opcode{BRK}();
-        }
-        system_register 'X', (system_register 'A') % (system_memory @_);
-        system_register 'A', int((system_register 'A') / (system_memory @_));
-        flag_test system_register 'A';
-    };
+    my $value = get_memory;
+    if ( 0 == $value ) {
+        system_flag('Z', 1);
+        system_flag('V', 1);
+        system_flag('B', 1);
+        $opcode{BRK}();
+    }
+    system_register 'X', (system_register 'A') % $value;
+    system_register 'A', int((system_register 'A') / $value);
+    flag_test system_register 'A';
+};
+
+sub load_memory {
+    system_memory( @_, 0, 0, 0 );
+};
+
+sub run_snippet {
+    @snippet = @_;
+    while ( @snippet && ! system_flag('B') ) {
+say "Doing $snippet[0]";
+        $opcode{shift @snippet}();
+    }
+}
 
 1;
 __END__
