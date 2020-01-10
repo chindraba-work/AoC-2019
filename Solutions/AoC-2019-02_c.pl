ff8ebb3b496bba0d0225b189b5b00e81fe04cf64
Ronald Lamoreaux <gitkey@chindraba.work>
2020-01-10 13:15:03 +0000

    Solution for part 2 of day 2 - brute force
    Decided to run it in brute force mode, to see the difference.
    
    Big difference!
    
    On average, the brute force method takes in excess of 11 seconds. The
    original version runs between 0.060 and 0.075 on average. I did have an
    extreme case that was 0.108 seconds. Two orders of magnitude is rather
    substantial.
    
    Signed-off-by: Ronald Lamoreaux <gitkey@chindraba.work>

---
+++ Solutions/AoC-2019-02_c.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+
+# SPDX-License-Identifier: MIT
+
+########################################################################
+#                                                                      #
+#  This file is part of the solution set for the programming puzzles   #
+#  presented by the 2019 Advent of Code challenge.                     #
+#  See: https://adventofcode.com/2019                                  #
+#                                                                      #
+#  Copyright © 2020  Chindraba (Ronald Lamoreaux)                      #
+#                    <aoc@chindraba.work>                              #
+#  - All Rights Reserved                                               #
+#                                                                      #
+#  Permission is hereby granted, free of charge, to any person         #
+#  obtaining a copy of this software and associated documentation      #
+#  files (the "Software"), to deal in the Software without             #
+#  restriction, including without limitation the rights to use, copy,  #
+#  modify, merge, publish, distribute, sublicense, and/or sell copies  #
+#  of the Software, and to permit persons to whom the Software is      #
+#  furnished to do so, subject to the following conditions:            #
+#                                                                      #
+#  The above copyright notice and this permission notice shall be      #
+#  included in all copies or substantial portions of the Software.     #
+#                                                                      #
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,     #
+#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF  #
+#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND               #
+#  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS #
+#  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN  #
+#  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN   #
+#  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    #
+#  SOFTWARE.                                                           #
+#                                                                      #
+########################################################################
+
+use 5.026001;
+use strict;
+use warnings;
+use IntCode::ElfComp;
+
+my $target_value = 19690720;
+
+for my $noun (0..99) {
+    for my $verb (0..99) {
+        load_code_file($main::data_file);
+        terminal_memory_access(1,$noun);
+        terminal_memory_access(2,$verb);
+        elf_launch();
+        if ( terminal_memory_access(0) == $target_value ) {
+            say 100 * $noun + $verb, " = 100 * $noun + $verb and gives ", terminal_memory_access(0);
+            exit;
+        }
+        warm_boot();
+    }
+}
+
+1;
