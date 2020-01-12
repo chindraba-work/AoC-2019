3efb3ac8276403cd9492e6186f12690b8c9de427
Ronald Lamoreaux <gitkey@chindraba.work>
2020-01-12 03:29:08 +0000

    Day 4 part 1 solution
    I can see the sequence of sums here, but coding it is too much trouble.
    A simple brute force approach is so easy to write that it's the method
    I'm going to use here. Part 2 of the challenge might make me wish I'd
    gone the mathematical route, but who knows.
    
    Signed-off-by: Ronald Lamoreaux <gitkey@chindraba.work>

---
+++ Solutions/AoC-2019-04_a.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+
+# SPDX-License-Identifier: MIT
+
+########################################################################
+#                                                                      #
+#  This file is part of the solution set for the programming puzzles   #
+#  presented by the 2019 Advent of Code challenge.                     #
+#  See: https://adventofcode.com/2019                                  #
+#                                                                      #
+#  Copyright © 2020  Chindraba (Ronald Lamoreaux)                      #
+#                    <aoc@chindraba.work>                              #
+#  - All Rights Reserved                                               #
+#                                                                      #
+#  Permission is hereby granted, free of charge, to any person         #
+#  obtaining a copy of this software and associated documentation      #
+#  files (the "Software"), to deal in the Software without             #
+#  restriction, including without limitation the rights to use, copy,  #
+#  modify, merge, publish, distribute, sublicense, and/or sell copies  #
+#  of the Software, and to permit persons to whom the Software is      #
+#  furnished to do so, subject to the following conditions:            #
+#                                                                      #
+#  The above copyright notice and this permission notice shall be      #
+#  included in all copies or substantial portions of the Software.     #
+#                                                                      #
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,     #
+#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF  #
+#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND               #
+#  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS #
+#  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN  #
+#  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN   #
+#  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    #
+#  SOFTWARE.                                                           #
+#                                                                      #
+########################################################################
+
+use 5.026001;
+use strict;
+use warnings;
+use Elves::GetData 'read_lines';
+my ($low_limit, $high_limit) = split '-', (read_lines("Data/AoC-2019-04.txt"))[0];
+
+my $count = 0;
+for my $a (1..9) {
+    for my $b ($a..9) {
+        for my $c ($b..9) {
+            for my $d ($c..9) {
+                for my $e ($d..9) {
+                    for my $f ($e..9) {
+                        my $num = join '', ($a,$b,$c,$d,$e,$f);
+                        if (
+                            ( $num >= $low_limit ) &&
+                            ( $num <= $high_limit ) &&
+                            ( $num =~ /([0-9])\1/ )
+                        ) {
+                            $count++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+say "From $low_limit to $high_limit has $count valid passwords.";
+
+1;
