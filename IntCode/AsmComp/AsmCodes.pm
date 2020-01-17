99beb78fe3da6e414cac1f579b2d45918a0074ee
Ronald Lamoreaux <gitkey@chindraba.work>
2020-01-17 14:34:09 +0000

    Make I/O adjustments for assembly codes
    Converted the output codes to use `push` rather than `unshift`. This
    keeps the command line arguments, if any, intact, and allows the next
    level up to access both the existing arguments and the return value in
    the same place. Use `shift` to get the next command line argument, and
    `pop` to get the last return value, with the ability to dig into the
    data even farther using multiple shift or pop commands.
    
    Working through the assembly codes I decided to document them, since I
    have deviated so far from the prototype of the 6502 chip's assembly
    codes. Doing so exposed a few places where I could add more codes for
    balance, and helped me to see a better way to communicate with the
    supervising code on "program" exit.
    
    In essence, the terminal conditions are recorded in three flags, and
    their binary sum is the return code from launch of the program. The
    requested data is returned in `ARGV` as everything else is now. The
    input and output from `STDIN` can now be faked, or piped, as can the
    output for `STDOUT`.
    
    Along the same lines, the routine names have been modified in a few
    cases. It helps to organize them, helps find breaking points in the
    calling modules, when I get to that, and helps to keep things in an
    arrangement between levels for lightened cognitive load.
    
    It is my hope that the operations of the AsmComp is done at this level
    and that the API and user interface can become the focus of development
    for the AsmComp.
    
    The ElfComp, and existing solutions still need testing, and possibly
    updates, to match the changes here.
    
    Signed-off-by: Ronald Lamoreaux <gitkey@chindraba.work>

--- IntCode/AsmComp/AsmCodes.pm
+++ IntCode/AsmComp/AsmCodes.pm
@@ -12,12 +12,12 @@ require Exporter;
 our @ISA = qw(Exporter);
 
 our %EXPORT_TAGS = ( 'all' => [ qw(
-	program_load
-	memory_load
+	code_launch
+	code_resume
+	code_step
 	direct_memory_access
-	program_resume
-	program_run
-	program_step
+	load_code
+	load_memory
 	one_shot
 	soft_start
 ) ] );
@@ -31,7 +31,25 @@ our $VERSION = '0.19.07';
 my $access_mode;
 my $operand;
 
-*program_load = *IntCode::AsmComp::BaseCode::program_init;
+*load_code = *IntCode::AsmComp::BaseCode::program_init;
+
+sub load_memory {
+    system_memory(@_, 0, 0, 0 );
+};
+
+sub direct_memory_access {
+    system_memory($addressing{Absolute}, $_[0], $_[1])
+        if ( 2 == @_ );
+    return system_memory($addressing{Absolute}, $_[0]);
+}
+
+sub set_memory {
+    system_memory($access_mode, $operand, $_[0]);
+}
+
+sub get_memory {
+    return system_memory($access_mode, $operand);
+}
 
 sub set_operand {
     $access_mode = $addressing{program_next_code()};
@@ -47,32 +65,8 @@ sub register_check {
     flag_test(system_register($_[0]));
 }
 
-sub load_memory {
-    system_memory(@_);
-};
-
-sub set_memory {
-    system_memory($access_mode, $operand, $_[0]);
-}
-
-sub get_memory {
-    return system_memory($access_mode, $operand);
-}
-
-sub direct_memory_access {
-    system_memory($addressing{Absolute}, $_[0], $_[1])
-        if ( 2 == @_ );
-    return system_memory($addressing{Absolute}, $_[0]);
-}
-
 sub memory_check {
     flag_test(system_memory($access_mode, $operand));
-}
-
-sub copy_register {
-    system_register($_[1], system_register($_[0]));
-    register_check($_[1])
-        if ( $_[2] );
 }
 
 sub load_register {
@@ -85,6 +79,12 @@ sub load_register {
 sub store_register {
     set_operand();
     set_memory(system_register($_[0]));
+}
+
+sub copy_register {
+    system_register($_[1], system_register($_[0]));
+    register_check($_[1])
+        if ( $_[2] );
 }
 
 sub branch {
@@ -204,20 +204,28 @@ my %asmcode = (
         system_memory($access_mode, $operand, $value + 1);
         memory_check();
     },
+    INI => sub {
+        system_register('I', system_register('I') + 1);
+        register_check('I');
+    },
     INP => sub {
         set_operand();
-        system_register('D', shift(@ARGV))
-            || die "Data expected but not found on command line.";
+        print "AsmComp input: ";
+        if ( @ARGV ) {
+            system_register('D', shift(@ARGV));
+            printf("%s\n", system_register('D'));
+        } else {
+            $| =1;
+            $_ = <STDIN>;
+            chomp;
+            system_register('D', $_);
+        }
         register_check('D');
         if ( $access_mode == $addressing{Accumulator} ) {
             system_register('A', system_register('D'));
         } else {
             system_memory($access_mode, $operand, system_register('D'));
         }
-    },
-    INI => sub {
-        system_register('I', system_register('I') + 1);
-        register_check('I');
     },
     INX => sub {
         system_register('X', system_register('X') + 1);
@@ -248,50 +256,42 @@ my %asmcode = (
         system_register('A', system_register('A') * get_memory());
         register_check('A');
     },
-    OPB => sub {
-        set_operand();
-        system_register('D', get_memory());
-        unshift(@ARGV, sprintf(
-            "%016b",
-            system_register('D')
-        ));
-    },
-    OPD => sub {
-        set_operand();
-        system_register('D', get_memory());
-        unshift(@ARGV, sprintf(
-            "%d",
-            system_register('D')
-        ));
-    },
-    OPH => sub {
-        set_operand();
-        system_register('D', get_memory());
-        unshift(@ARGV, sprintf(
-            "%08X",
-            system_register('D')
-        ));
-    },
-    OPO => sub {
-        set_operand();
-        system_register('D', get_memory());
-        unshift(@ARGV, sprintf(
-            "%#o",
-            system_register('D')
-        ));
-    },
-    OUT => sub {
-        set_operand();
-        system_register('D', get_memory());
-        unshift(@ARGV, sprintf(
-            "Program output: %1\$016b: %1\$u, (%1\$d)",
-            system_register('D')
-        ));
-    },
     ORA => sub {
         set_operand();
         system_register('A', system_register('A') | get_memory());
         register_check('A');
+    },
+    OTB => sub {
+        set_operand();
+        system_register('D', get_memory());
+        push(@ARGV, sprintf(
+            "%016b",
+            system_register('D')
+        ));
+    },
+    OTD => sub {
+        set_operand();
+        system_register('D', get_memory());
+        push(@ARGV, sprintf(
+            "%d",
+            system_register('D')
+        ));
+    },
+    OTH => sub {
+        set_operand();
+        system_register('D', get_memory());
+        push(@ARGV, sprintf(
+            "%08X",
+            system_register('D')
+        ));
+    },
+    OTO => sub {
+        set_operand();
+        system_register('D', get_memory());
+        push(@ARGV, sprintf(
+            "%#o",
+            system_register('D')
+        ));
     },
     PHA => sub { system_stack(system_register('A')); },
     PHP => sub { system_stack(system_register('F')); },
@@ -319,22 +319,14 @@ my %asmcode = (
         set_operand();
         system_register('D', get_memory());
         printf(
-            "Program output: %1\$016b: %1\$u, (%1\$d)\n",
+            "%s\n",
             system_register('D')
         );
     },
     REA => sub {
         set_operand();
-        print "AsmComp input: ";
-        if ( @ARGV ) {
-            system_register('D', unshift(@ARGV));
-            printf("%s\n", system_register('D'));
-        } else {
-            $| =1;
-            $_ = <STDIN>;
-            chomp;
-            system_register('D', $_);
-        }
+        system_register('D', shift(@ARGV))
+            || die "Data expected but not found on command line.";
         register_check('D');
         if ( $access_mode == $addressing{Accumulator} ) {
             system_register('A', system_register('D'));
@@ -360,6 +352,14 @@ my %asmcode = (
         }
         memory_check();
     },
+    RPT => sub {
+        set_operand();
+        system_register('D', get_memory());
+        unshift(@ARGV, sprintf(
+            "Program output: %1\$016b: %1\$#o: %1\$08X: %1\$u: (%1\$d)",
+            system_register('D')
+        ));
+    },
     RTI => sub {
         system_register('F', system_stack());
         system_register('C', system_stack());
@@ -375,6 +375,7 @@ my %asmcode = (
     SEC => sub { system_flag('C', 1); },
     SED => sub { system_flag('D', 1); },
     SEI => sub { system_flag('I', 1); },
+    SEV => sub { system_flag('V', 1); },
     SEX => sub { system_flag('X', 1); },
     STA => sub { store_register('A'); },
     STD => sub { store_register('D'); },
@@ -387,11 +388,13 @@ my %asmcode = (
     STX => sub { store_register('X'); },
     TAD => sub { copy_register('A', 'D'); },
     TAI => sub { copy_register('A', 'I'); },
+    TAS => sub { copy_register('A', 'S'); },
     TAX => sub { copy_register('A', 'X', 1); },
     TDA => sub { copy_register('D', 'A', 1); },
     TDX => sub { copy_register('D', 'X', 1); },
     TIA => sub { copy_register('I', 'A', 1); },
     TIX => sub { copy_register('I', 'X', 1); },
+    TSA => sub { copy_register('S', 'A', 1); },
     TSX => sub { copy_register('S', 'X', 1); },
     TXA => sub { copy_register('X', 'A', 1); },
     TXD => sub { copy_register('X', 'D'); },
@@ -413,10 +416,6 @@ $asmcode{DIV} = sub {
     register_check('A');
 };
 
-sub memory_load {
-    load_memory(@_, 0, 0, 0 );
-};
-
 sub run_code {
     while ( ! system_flag('I') && ! system_flag('B') && system_register('C') < program_length() ) {
         $asmcode{program_next_code()}();
@@ -435,34 +434,39 @@ sub soft_start {
         system_flag($_, 0);
     } qw(N V B I Z C);
     system_memory( (0) x4 );
+    if ( @_ ) {
+        program_load(@_);
+    } else {
+        program_load( (0) x 4 );
+    }
 }
 
 sub code_execute {
     system_flag('X', 0);
     run_code();
-    return system_register('D') if ( system_flag('X') );
-    return 1 if ( system_flag('B') );
-    return 0 if ( system_flag('I') );
+    push(@ARGV, (system_register('D'))
+        if ( system_flag('X') );
+    return (system_register('F') >> 2) & 7)
+        if ( system_flag('B') || system_flag('I') || system_flag('X') );
     return undef;
 }
 
-sub program_run {
+sub code_launch {
     map {
         system_register($_, 0);
     } qw(A C I S X D);
-    code_execute();
+    return code_execute();
 }
 
-sub program_resume {
+sub code_resume {
     $asmcode{'RTI'}();
-    code_execute();
+    return code_execute();
 }
 
 sub program_step {
-    return program_run() 
+    return code_launch() 
         if ( 0 == system_register('S') );
-    $asmcode{'RTI'}();
-    code_execute();
+    return code_resume();
 }
 
 1;
@@ -481,16 +485,211 @@ IntCode::AsmComp::AsmCodes
 Implementation of the assembly-level workings of the IntCode computer
 needed by the elves in the 2019 Advent of Code challenges.
 
+Exported routines are:
+    code_launch()
+        Clear the registers and launch the program.
+        Return value:
+            0: program terminated by reaching end of command list
+            1: program terminated with BRK command
+            2: program terminated with STP command
+            3: program terminated with divide by zero error
+            4: program terminated by reaching end of command list, and
+                D register contents pushed onto ARGV
+            5: program terminated with BRK command, and
+                D register contents pushed onto ARGV
+            6: program terminated with STP command, and
+                D register contents pushed onto ARGV
+            7: program terminated with divide by zero error, and
+                D register contents pushed onto ARGV
+    code_resume()
+        Perform and RTI instruction (pop status register and code
+            pointer) and begin executing instructions at the new code
+            pointer position.
+        Return value: see code_launch
+    code_step()
+        Based on the value of the Stack Pointer, either do a code_launch
+            or code_resume. If the Stack Pointer indicates that there
+            is data on the stack, code_resume is the choice. So long
+            as the snippets do not place data on the stack, this allows
+            for a set of snippets to be executed while retaining the
+            status of all the flags, and clearing all the registers.
+            There is no provisions to run snippets in series while
+            retaining register values as well. To do this requires that
+            the snippets take control over the data in the registers,
+            using memory space between invocations to hold their data.
+        Return value: see code_launch
+    direct_memory_access(memory_address[, new-data])
+        Routine to allow supervising programs to directly read and write
+            single addresses within the data segment. The first argument
+            is the absolute address to access. There is not method for
+            using relative, indexed, or pointer address modes. The 
+            second argument, if any, is the raw data to write to the
+            indicated memory address. It is possible to use this to put
+            strings or floating point numbers into the "computer" memory
+            at the address. However, if that is accessed directly by
+            "running" program, errors are likely, as the computer is
+            designed to deal with integers data only.
+        The exception to dealing with non-interger data in memory is the
+            PRT command, which performs no processing on the data, and
+            simply reads the memory and prints it to <STDOUT> and can
+            handle strings, or any other basic data type.
+        The one-argument version is to read memory, and the two-argument
+            version is to write to memory
+        Return value:
+            The current contents of the address given.
+    load_code(program_code_list)
+        Directly load the program into memory. The raw contents of the
+            argument list is copied into the code segment of the
+            "computer". No checks or tests of any kind are performed.
+        Return value: none
+    load_memory(memory_data_list)
+        Directly load the data into memory. The raw contents of the
+            argument list is copied into the data segment of the
+            "computer". No checks or tests of any kind are performed.
+        Return value: none
+    one_shot(CMD[, operand[, operand]])
+        Allows the execution of exactly one assembly code command. The
+            call must include the data for the command, if any. Setting
+            and clearing flags, and pre-loading registers for testing
+            are the primary purposes for this routine's use.
+        Return value: none
+    soft_start([program_code_list])
+        Clears the registers, except the status register, and flags,
+            expect for the Decimal and X flags, wipes the data segment
+            and code segment. Nearly the same as relaunching the script,
+            except that the X and Decimal flags, used by external code
+            to control certain funtionality of the interface, are kept
+            as set by the supervising code, if any.
+        Return value: none
+
+The AsmCodes, or assembly codes and what the mean. or do, are
+    ABT: Abort
+    ADC: Add with carry, add memory to accumulator
+    AND: Logical AND accumulator with memory
+    ASL: Arithmetic shift left, accumulator one bit to the left
+    ASR: Arithmetic shift right, accumulator one bit to the right
+    BCC: Branch if Carry flag clear
+    BCS: Branch if Carry flag set
+    BEQ: Branch if Zero flag set
+    BIT: Bit manipulation of the memory
+    BMI: Branch if Negative flag set
+    BNE: Branch if Zero flag clear
+    BPL: Branch if Negative flag clear
+    BRK: Break- Push code pointer and status register, set interrupt
+    BVC: Branch if Overflow flag clear
+    BVS: Branch if Overflow flag set
+    CLC: Clear Carry flat
+    CLD: Clear Decimal flag
+    CLI: Clear Interrupt flag
+    CLV: Clear Overflow flag
+    CLX: Clear X flag
+    CMP: Compare accumulator with memory
+    CPX: Compare X register with memory
+    DEC: Decrement memory
+    DEI: Decrement Index register
+    DEX: Decrement X register
+    DIV: Divide accumulator by memory
+    EOR: Exclusive Or accumulator with memory
+    GET: Blocking read of ARGV into memory, see REA
+    INC: Increment memory
+    INI: Increment Index register
+    INP: Prompting read of STDIN, reads ARGV if present instead
+    INX: Increment X register
+    JMP: Jump to address
+    JST: Subroutine jump- push code pointer, jump to address
+    LDA: Load accumulator from memory
+    LDD: Load D register from memory
+    LDF: Load Status register from memory
+    LDI: Load Index register from memory
+    LDS: Load Stack pointer from memory
+    LDX: Load X register from memory
+    LSR: Logical shift right
+    MUL: Multiply accumulator by memory
+    ORA: Bitwise OR accumulator with memory
+    OTB: Push memory value as binary number string on ARGV
+    OTD: Push memory value as signed decimal number string on ARGV
+    OTH: Push memory value as hex number string on ARGV
+    OTO: Push memory value as octal number string on ARGV
+    PHA: Push accumulator onto stack
+    PHP: Push status register onto stack
+    PHV: Push memory onto ARGV
+    PLA: Pop accumulator from stack
+    PLP: Pop status register from stack
+    PLV: Pop memory from stack
+    PRT: Print memory to STDOUT
+    REA: Manditory pop ARGV into memory, halts program on failure, see GET
+    ROL: Rotate memory left
+    ROR: Rotate memory right
+    RPT: Push formatted output string onto ARGV
+    RTI: Return from interrupt- pop system register, pop code pointer
+    RTS: Return from subroutine- pop code pointer
+    SBC: Subtract memory from accumulator
+    SEC: Set Carry flag
+    SED: Set Decimal flag
+    SEI: Set Interrupt flag
+    SEV: Set Overflow flag
+    SEX: Set X flag
+    STA: Store accumulator in memory
+    STD: Store D Register in memory
+    STI: Store Index register in memory
+    STP: Stop- set Break flag, end program
+    STX: Store X register in memory
+    TAD: Transfer accumulator to D register
+    TAI: Transfer accumulator to Index register
+    TAS: Transfer accumulator to Stack pointer
+    TAX: Transfer accumulator to X register
+    TDA: Transfer D register to accumulator
+    TDX: Transfer D register to X register
+    TIA: Transfer Index Pointer to accumulator
+    TIX: Transfer Index Pointer to X register
+    TSA: Transfer Stack Pointer to accumulator
+    TSX: Transfer Stack Pointer to X register
+    TXA: Transfer X register to accumulator
+    TXD: Transfer X register to D register
+    TXI: Transfer X register to Index register
+    TXS: Transfer X register to Stack Pointer
+
+The codes, grouped by function:
+    Flag controls:
+        Clear Flag:
+            CLC, CLD, CLI, CLV, CLX
+        Set Flag:
+            SEC, SED, SEI, SEV, SEX
+        Tests:
+            CMP, CPX
+    Register controls:
+        Load register:
+            LDA, LDD, LDF, LDI, LDS, LDX, PLA, PLP
+        Store register:
+            PHA, PHP, STA, STD, STI, STX
+        Transfer register:
+            TAD, TAI, TAS, TAX, TDA, TDX, TIA, TIX, TSA, TSX, TXA, TXD, TXI, TXS
+    Flow controls:
+        BCC, BCS, BEQ, BMI, BNE, BPL, BVC, BVS, JMP, JST, RTI, RTS
+    Program controls:
+        ABT, BRK, STP
+    Bit manipulations:
+        ASL, ASR, BIT, LSR, ROL, ROR
+    Logical operations:
+        AND, EOR, ORA
+    Math operations:
+        ADC, DEC, DEI, DEX, DIV, INC, INI, INX, MUL, SBC
+    I/O operations
+        Input:
+            GET, INP, PLV, REA
+        Output:
+            OTB, OTD, OTH, OTO, PHV, PRT, RPT
+
 =head2 EXPORT
 
-	program_load
-	memory_load
-    direct_memory_access
-    program_resume
-	program_run
-    program_step
-    one_shot
-    soft_start
+	code_launch
+	code_resume
+	code_step
+	direct_memory_access
+	load_code
+	load_memory
+	one_shot
+	soft_start
 
 =head1 AUTHOR
 
